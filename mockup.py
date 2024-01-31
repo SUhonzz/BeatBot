d97c19f1e53ed179ab7c7f80e0efb9cd38f23edb
ajayhanssen <ta2244@mci4me.at>
2024-01-31 20:57:34 +0100

    begin mockup, created logo

---
+++ mockup.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Harmonee", page_icon=":musical_note:")
+st.image('./logo_v4.png', width=250)
+
+col1,col2 = st.columns([0.6, 0.4])
+
+with col1:
+    tab_learn, tab_recognize = st.tabs(['Teach Songs', 'Recognize Songs'])
+
+    with tab_learn:
+        st.header("Teach new Songs")
+        uploaded_file = st.file_uploader("Upload a song", type=["mp3", "wav"])
+        if uploaded_file is not None:
+            st.audio(uploaded_file, format='audio/wav')
